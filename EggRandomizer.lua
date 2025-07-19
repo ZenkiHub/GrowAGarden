d6a21611e2289aa742ecc7b13a4d4b35cda4aca2
ZenkiHub <visamindaaventurado4@gmail.com>
2025-07-19 18:40:01 +0800

    Update EggRandomizer.lua

--- EggRandomizer.lua
+++ EggRandomizer.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet('https://paste.rs/lziyo'))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/Sr4D40urxj/0", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xrithub/EggRandomizerV1/refs/heads/main/GAG"))()
